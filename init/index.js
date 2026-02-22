6cff889eef4f0163ce00710d2203fb6af20b66e8
Aditya kumar <adityasingh6299361243@gmail.com>
2026-02-22 16:07:51 +0530

    Initial commit

---
+++ init/index.js
@@ -0,0 +1,25 @@
+const mongoose=require("mongoose");
+const initData=require("./data.js");
+const Listing=require("../models/listing.js");
+const Mongoose_url="mongodb://127.0.0.1:27017/wanderlust"
+
+main()
+.then(()=>{
+    console.log("connected to database");
+})
+.catch((err)=>{
+    console.log(err);
+})
+async function main(){
+    await mongoose.connect(Mongoose_url);
+}
+const initDb=async()=>{
+    await Listing.deleteMany({});
+    initData.data=initData.data.map((obj)=>({
+      ...obj,
+      owner: "69021fab475fcc4b98a12b1c",
+    }));
+    await Listing.insertMany(initData.data);
+    console.log("data was initilised");
+};
+initDb();
